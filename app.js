454c460f5b4486d8e22f8f1c35adc642a491664e
Ivy Molina <62899870+premolina492@users.noreply.github.com>
2022-11-14 18:11:47 -0800

    erased forecast function due to not functioning properly

--- app.js
+++ app.js
@@ -23,57 +23,11 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function formatDay(timestamp) {
-    let date = new Date(timestamp * 1000);
-    let day = date.getDay();
-    let days = ["Sun", "Mon", "Tues", "Wednes", "Thurs", "Fri", "Sat"];
-
-    return days[day];
-
-}
-
-function displayForecast(response) {
-    let forecast = response.data.daily;
-    let forecastElement = document.querySelector("#forecast");
-
-
-    let forecastHTML = `<div class="row">`;
-    forecast.forEach(function (forecastDay, index) {
-        if (index < 6) {
-        forecastHTML = forecastHTML + 
-            `
-            <div class="col-2">
-            <div class="weather-forecast-date">${formatDay(forecastDay.timestamp)}</div>
-            <img 
-                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" 
-                alt="" 
-                width="42" 
-            />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max">  ${Math.round(forecastDay.temp.max)}° </span>
-            <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
-      </div>
-      </div>
-        `;
-        }
-    });
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
-}
-
-function getForecast(coordinates) {
-    console.log(coordinates);
-    let apiKey = "233ae3do44b912208eba49363ftbb5fe";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}`
-    axios.get(apiUrl).then(displayForecast);
-}
-
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon")
@@ -83,7 +37,6 @@ function displayTemperature(response) {
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
-    humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.time * 1000);
     iconElement.setAttribute(
@@ -92,7 +45,6 @@ function displayTemperature(response) {
         );
    iconElement.setAttribute("alt", response.data.condition.description);
 
- getForecast(response.data.coordinates);
 
 }
 
